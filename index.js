b3df6f132e2c6be1b4936a4755fe51fe18100371
pius ssekweyama <ssekweyamapius@gmail.com>
2025-01-21 18:03:32 +0300

    initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//window.alert('Welcome to the Sports Page!');
+let user = 245696;
+
+const sportswelcome = document.getElementById("sportsButton");
+
+sportswelcome.addEventListener('click', function() {
+
+    document.getElementById("teamname").textContent = "Swarmbyte FC";
+
+}); 
+
