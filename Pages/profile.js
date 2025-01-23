4dbe01daa8f589479ea0e4480166547a75a0e63b
Marc Anthony <marckamanzi64@gmail.com>
2025-01-23 16:41:58 +0300

    type conversion for age from string to number, and displaying next age

--- Pages/profile.js
+++ Pages/profile.js
@@ -3,18 +3,23 @@ let Email;
 let Age;
 let Username;
 
+
 document.getElementById("submitbutton").onclick = function () {
+
     Email = document.getElementById("emailinput").value;
-    Age = document.getElementById("age").value;
+    Age = Number (document.getElementById("age").value);
     Username = document.getElementById("usernameinput").value;
 
     console.log(Email);
-    console.log(Age);
+    console.log(Age, typeof Age);
     console.log(Username);
 
     document.getElementById("user").textContent = Username;
     document.getElementById("submittedage").textContent = Age;
     document.getElementById("useremail").textContent = Email;
+
+   
+    window.alert(`Welcome to kapeesa ` + Username + ` You have registered successfully`);
+
+    window.alert(`You turn ${Age +1} years old on your next birthday`);
 }
-
-
